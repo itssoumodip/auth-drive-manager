d0c4d02db7c9a0375ff3310bc05a6660c58bbf20
itssoumodip <soumodipddas2005@gmail.com>
2025-04-07 23:40:43 +0530

    Created user routes with a test endpoint.

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import userRouter from './routes/user.routes.js';
+
+const app = express();
+   
+app.set('view engine', 'ejs');
+
+app.use('/user', userRouter);
+
+app.listen(3005, () => {
+    console.log('Server is running on port 3005');
+})
