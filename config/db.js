439c298970f2babb7deb2765fb1d6a722cc6e4eb
itssoumodip <soumodipddas2005@gmail.com>
2025-04-12 03:54:33 +0530

    Enhance MongoDB connection with options for new URL parser and unified topology

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
 function connectToDB() {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
         console.log("MongoDB connected successfully");    
-    })
+    }).catch((err) => {
+        console.error("MongoDB connection failed:", err);
+    });
 }
 
 export default connectToDB;
